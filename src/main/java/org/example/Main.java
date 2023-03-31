133ca9a77d7cc7f51486a4e5fba2d48d99c5ec26
Hakan Duygu <hakkoman9@hotmail.com>
2023-03-31 11:00:52 +0300

    gurkan bos adam

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("chello world33333!");
+        System.out.println("chello world 33333!");
     }
 }
